173de2c3ebbf003791e0862d1d8586ae7bfb02c9
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-12-15 21:23:53 +0300

    Editor Upload

--- lib/screens/editorUploadProduct.dart
+++ lib/screens/editorUploadProduct.dart
@@ -1,12 +1,15 @@
 import 'package:ecommerce_flutter_admin/constans/app_constans.dart';
+import 'package:ecommerce_flutter_admin/models/product_model.dart';
 import 'package:ecommerce_flutter_admin/services/assets_manager.dart';
 import 'package:ecommerce_flutter_admin/widget/title_text.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class EditorUploadProductScreen extends StatefulWidget {
   static const routName = "/EditorUploadProductScreen";
 
-  const EditorUploadProductScreen({super.key});
+  const EditorUploadProductScreen({super.key, this.productModel});
+  final ProductModel? productModel;
 
   @override
   State<EditorUploadProductScreen> createState() => _EditorUploadProductScreenState();
@@ -14,6 +17,59 @@ class EditorUploadProductScreen extends StatefulWidget {
 
 class _EditorUploadProductScreenState extends State<EditorUploadProductScreen> {
   final _formKey = GlobalKey<FormState>();
+  XFile? _pickedImage;
+  late TextEditingController  _titleController, _priceController, _descriptionController,
+  _quanttiyContoller;
+  String? _categoryValue;
+  bool isEditing = false;
+  String? productNetworkImage;
+
+  @override
+  void initState(){
+    if(widget.productModel !=null)
+    {
+      isEditing =true;
+      productNetworkImage = widget.productModel!.productImage;
+      _categoryValue= widget.productModel!.productCategory;
+
+
+    }
+
+    _titleController = TextEditingController(text:widget.productModel?.productTitle);
+    _priceController = TextEditingController(text:widget.productModel?.productPrice);
+    _descriptionController = TextEditingController(text:widget.productModel?.productDescription);
+    _quanttiyContoller = TextEditingController(text:widget.productModel?.productQuantity);
+
+    super.initState();
+
+    @override
+    void dispose(){
+      _titleController.dispose();
+      _priceController.dispose();
+      _descriptionController.dispose();
+      _quanttiyContoller.dispose();
+
+      super.dispose();
+    }
+    void removePickedImage(){
+      setState(() {
+        _pickedImage= null;
+        productNetworkImage =null;
+      });
+    }
+
+    void clearForm(){
+      _titleController.clear();
+      _priceController.clear();
+      _descriptionController.clear();
+      _quanttiyContoller.clear();
+      removePickedImage();
+    }
+
+
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return  GestureDetector(
