077495dcbdc25bf7abcf8add79728a8d47759e0a
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-12-17 15:26:41 +0300

    Firebase step 3

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:ecommerce_flutter_admin/screens/DashboardScreen.dart';
 import 'package:ecommerce_flutter_admin/screens/editorUploadProduct.dart';
 import 'package:ecommerce_flutter_admin/screens/search_screen.dart';
 import 'package:ecommerce_flutter_admin/widget/order/order_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -18,6 +19,37 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder<FirebaseApp>
+    (future: Firebase.initializeApp(
+    options: const FirebaseOptions(
+    apiKey: 'AIzaSyAZ7Ab-PfGPcIJWLD70n8prT_tKZBVY154',
+    appId: '1:375993132013:web:173caaec9586c7f44bfde4',
+    messagingSenderId: '375993132013',
+    projectId: 'ecommerce-udemy-ff943',
+    )
+    ),
+    builder: (context, snapshot){
+    if(snapshot.connectionState== ConnectionState.waiting){
+    return const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home:Scaffold(
+    body: Center(
+    child: CircularProgressIndicator(),
+    ),
+    )
+    );
+    }
+    else if(snapshot.hasError){
+    return  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home:Scaffold(
+    body: Center(
+    child: SelectableText(snapshot.error.toString()),
+    ),
+    )
+    );
+
+    }
     return MultiProvider(providers: [
       ChangeNotifierProvider(create: (_){
         return ThemeProvider();
@@ -40,11 +72,13 @@ class MyApp extends StatelessWidget {
             SearchScreen.routName: (context) => const SearchScreen(),
             EditorUploadProductScreen.routName: (context) => const EditorUploadProductScreen(),
           },
-
       );
 
 
+        }),
+
+    );
     }
-    ));
+    );
   }
 }
